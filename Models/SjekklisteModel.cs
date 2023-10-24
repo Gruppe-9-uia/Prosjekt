3c6e2c110ed1853276a1c8c8746f937c99aeb79a
Andrine Celine Flatby <andrine.celine.flatby@gmail.com>
2023-10-24 17:19:19 +0200

    Laget model for generelt av sjekklisten

---
+++ Models/SjekklisteModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace Prosjekt.Models
+{
+    public class SjekklisteModel
+    {
+        public string Document_nr_str { get; set; }
+        public string Serial_number_str { get; set; }
+        public string Type_str { get; set; }
+        public string Procedure_str { get; set; }
+        public DateOnly Starting_Date { get; set; }
+        public string Prepared_by_str { get; set; }
+        public string xx_Bar_str { get; set; }
+        public string Traction_force_Kn { get; set; }
+        public string comment_str {  get; set; }
+    }
+}
