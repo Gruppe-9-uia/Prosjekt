ff881e66743cd6db017dd0e3032f23e0401ada20
Andrine Celine Flatby <andrine.celine.flatby@gmail.com>
2023-10-24 19:39:26 +0200

    satt alt i en model siden view lar bare en model omgang

--- Models/ElectroModel.cs
+++ Models/ElectroModel.cs
@@ -2,5 +2,11 @@
 {
     public class ElectroModel
     {
+        public string Document_nr_str { get; set; }
+        public string Department_ID { get; set; }
+        public string Wiring_on_winch { get; set; }
+        public string Pg_test_radio { get; set; }
+        public string Oil_gearbox { get; set; }
+        public string Button_box { get; set; }
     }
 }
--- Models/SjekklisteModel.cs
+++ Models/SjekklisteModel.cs
@@ -7,6 +7,7 @@ namespace Prosjekt.Models
     {
         public string Document_nr_str { get; set; }
         public string Serial_number_str { get; set; }
+        public string Department_ID { get; set; }
         public string Type_str { get; set; }
         public string Procedure_str { get; set; }
         public DateOnly Starting_Date { get; set; }
@@ -16,5 +17,27 @@ namespace Prosjekt.Models
         public string Traction_force_Kn { get; set; }
         public string Test_winch { get; set; }
         public string comment_str {  get; set; }
+
+        //Hydraulic model
+        public string Hydraulic_block { get; set; }
+        public string Oil_tank { get; set; }
+        public string Oil_gearbox_box { get; set; }
+        public string Ringe_cylinder_and_replace_seals { get; set; }
+
+        //Electro model
+        public string Wiring_on_winch { get; set; }
+        public string Pg_test_radio { get; set; }
+        public string Oil_gearbox { get; set; }
+        public string Button_box { get; set; }
+
+        //Mechanical model
+        public string Clutch_Plate { get; set; }
+        public string Brakes { get; set; }
+        public string Bearing_drum { get; set; }
+        public string PTO_and_storage { get; set; }
+        public string Chain_tensioners { get; set; }
+        public string Wire { get; set; }
+        public string Pinion_bearing { get; set; }
+        public string Wedge_on_sprocket { get; set; }
     }
 }
