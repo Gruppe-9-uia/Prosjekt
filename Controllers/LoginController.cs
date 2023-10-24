d3deb49edea56bada137b354edb0bf7277b52e35
Johannekh <johanne.haakenstad@icloud.com>
2023-10-24 21:01:38 +0200

    Controller og modell til Login

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,12 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Prosjekt.Models;
 
-namespace Prosjekt.Controllers
+public class AccountController : Controller
 {
-    public class LoginController : Controller
+    private const string IsLoggedInCookie = "IsLoggedIn";
+
+    [HttpGet]
+    public ActionResult Login()
     {
-        public IActionResult Login()
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Login(LoginModel user)
+    {
+        // Forenklet logikk: bruker alltid samme brukernavn og passord
+        // I en ekte applikasjon ville du kanskje sjekke dette mot en database
+        if (user.Username == "test" && user.Password == "passord")
         {
-            return View();
+            // Opprett en cookie for å indikere at brukeren er logget inn
+            Response.Cookies.Append(IsLoggedInCookie, "true");
+
+            return RedirectToAction("Welcome");
         }
+
+        ModelState.AddModelError("", "Invalid username or password.");
+        return View();
+    }
+
+    public ActionResult Logout()
+    {
+        // Fjern cookien
+        Response.Cookies.Delete(IsLoggedInCookie);
+
+        return RedirectToAction("Login");
+    }
+
+    public ActionResult Welcome()
+    {
+        // Sjekk cookien for å avgjøre om brukeren er logget inn
+        var isLoggedIn = Request.Cookies[IsLoggedInCookie];
+        if (string.IsNullOrEmpty(isLoggedIn) || isLoggedIn != "true")
+        {
+            return RedirectToAction("Login");
+        }
+
+        return View();
     }
 }
