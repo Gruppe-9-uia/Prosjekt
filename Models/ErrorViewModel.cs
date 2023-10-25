7709b598cc17dc3bf74a30ef93911b5bd58d2a63
Ine Antonsen <112353170+ineant@users.noreply.github.com>
2023-10-25 16:35:16 +0200

    Update ErrorViewModel.cs
    fjernet kommentar

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,9 +1,9 @@
-//namespace Prosjekt.Models
-//{
-    //public class ErrorViewModel
-   // {
-     //   public string? RequestId { get; set; }
+namespace Prosjekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-       // public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    //}
-//}
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
