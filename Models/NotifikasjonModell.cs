9f3d79a88e6389910d2e52d17cb6581521c6c60d
Vinh Vu Ai Nguyen <vinhvuainguyen@Vinhs-MacBook-Air.local>
2023-10-25 12:14:55 +0200

    Ny

---
+++ Models/NotifikasjonModell.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Prosjekt.Models
+{
+    public class Notifikasjon
+    {
+        public int Id { get; set; }
+        public string Melding { get; set; }
+        public string Status { get; set; }
+        public string Avdeling { get; set; }
+        public bool IsBookmarked { get; set; }
+    }
+
+}
+
+
+
