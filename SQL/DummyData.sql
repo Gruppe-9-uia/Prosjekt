999a88b98ffda69a1103071debbeb9e319c9d4a8
mariaathoang <mahoang@uia.no>
2023-11-09 13:20:51 +0100

    la til customer i dummydata

--- SQL/DummyData.sql
+++ SQL/DummyData.sql
@@ -27,3 +27,25 @@ INSERT INTO Employee (ID_int, FirstName_str, LastName_str, Phone_str, Email_str,
 VALUES (9, "Terje", "Gjøsæter", "+47 555 65 444", "TerjeG@mail.com", "bx8WOqyzDqS", "high", 3);
 INSERT INTO Employee (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Password_str, Level_str, DepartmentID_int)
 VALUES (10, "Rania", "El-Gazzar", "+47 555 65 555", "RaniaE@mail.com", "ZA7Q118LU", "high", 1);
+
+-- Customer
+INSERT INTO Customer (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Street_Address_str, Postal_Code_str)
+VALUES (1, "Dolly", "Barrett", "+47 410 00 001", "DollyRoberts@mail.com", "The Streets 1", "0001");
+INSERT INTO Customer (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Street_Address_str, Postal_Code_str)
+VALUES (2, "Saul", "Walsh", "+47 410 00 002", "SaulWalsh@mail.com", "The Streets 2", "0002");
+INSERT INTO Customer (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Street_Address_str, Postal_Code_str)
+VALUES (3, "Jessie", "Vega", "+47 410 00 003", "JessieVega@mail.com", "The Streets 3", "0003");
+INSERT INTO Customer (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Street_Address_str, Postal_Code_str)
+VALUES (4, "Morris", "Carson", "+47 410 00 004", "MorrisCarson@mail.com", "The Streets 4", "0004");
+INSERT INTO Customer (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Street_Address_str, Postal_Code_str)
+VALUES (5, "Kelly", "Stephens", "+47 410 00 005", "KellyStephens@mail.com", "The Streets 5", "0005");
+INSERT INTO Customer (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Street_Address_str, Postal_Code_str)
+VALUES (6, "Buddy", "Lutz", "+47 410 00 006", "BuddyLutz@mail.com", "The Streets 6", "0006");
+INSERT INTO Customer (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Street_Address_str, Postal_Code_str)
+VALUES (7, "Edward", "Medina", "+47 410 00 007", "EdwardMedina@mail.com", "The Streets 7", "0007");
+INSERT INTO Customer (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Street_Address_str, Postal_Code_str)
+VALUES (8, "Jody", "Haney", "+47 410 00 008", "JodyHaney@mail.com", "The Streets 8", "0008");
+INSERT INTO Customer (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Street_Address_str, Postal_Code_str)
+VALUES (9, "Greg", "Brown", "+47 410 00 009", "GregBrown@mail.com", "The Streets 9", "0009");
+INSERT INTO Customer (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Street_Address_str, Postal_Code_str)
+VALUES (10, "Kris", "Parrish", "+47 410 00 010", "KrisParrish@mail.com", "The Streets 10", "0010");
