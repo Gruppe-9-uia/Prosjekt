90b10df419f2518c67e0fca29eb364951865e848
ineant <inea@uia.no>
2023-11-09 13:02:20 +0100

    lagt til dummyData til Emplye, fikset en ny mappe for å samle alle SQL scriptene

---
+++ SQL/DummyData.sql
@@ -0,0 +1,29 @@
+-- Department
+INSERT INTO Department VALUES(1, "Admin");
+INSERT INTO Department VALUES(2, "Mekanisk");
+INSERT INTO Department VALUES(3, "Hydraulisk");
+INSERT INTO Department VALUES(4, "Elektro");
+
+
+
+-- Employees
+INSERT INTO Employee (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Password_str, Level_str, DepartmentID_int)
+VALUES (1, "Taylor", "Swift", "+47 452 65 333", "TaylorSwift@mail.com", "if51iP6Np", "high", 3);
+INSERT INTO Employee (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Password_str, Level_str, DepartmentID_int)
+VALUES (2, "Justin", "Bieber", "+47 452 65 444", "JustinBieber@mail.com", "BDXVSRLV9", "middel", 4);
+INSERT INTO Employee (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Password_str, Level_str, DepartmentID_int)
+VALUES (3, "Snoop", "Dog", "+47 452 65 555", "SnoopDog@mail.com", "yMFEWYvYD", "low", 1);
+INSERT INTO Employee (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Password_str, Level_str, DepartmentID_int)
+VALUES (4, "Ed", "Sheeran", "+47 452 65 666", "EdSheeran@mail.com", "vOuI0RVTB", "high", 2);
+INSERT INTO Employee (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Password_str, Level_str, DepartmentID_int)
+VALUES (5, "Julie", "Robin", "+47 452 65 777", "JulieRobin@mail.com", "Ki$Sdio/q", "high", 2;
+INSERT INTO Employee (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Password_str, Level_str, DepartmentID_int)
+VALUES (6, "Geir", "Hausvik", "+47 452 65 888", "GeirH@mail.com", "d8rOdlCjc", "low", 1);
+INSERT INTO Employee (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Password_str, Level_str, DepartmentID_int)
+VALUES (7, "Espen", "Limi", "+47 452 65 999", "EspenL@mail.com", "Wg0372MV6", "low", 3);
+INSERT INTO Employee (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Password_str, Level_str, DepartmentID_int)
+VALUES (8, "Sofie", "Wass", "+47 555 65 333", "SofieW@mail.com", "bx8WOqyzDqS", "middel", 4);
+INSERT INTO Employee (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Password_str, Level_str, DepartmentID_int)
+VALUES (9, "Terje", "Gjøsæter", "+47 555 65 444", "TerjeG@mail.com", "bx8WOqyzDqS", "high", 3);
+INSERT INTO Employee (ID_int, FirstName_str, LastName_str, Phone_str, Email_str, Password_str, Level_str, DepartmentID_int)
+VALUES (10, "Rania", "El-Gazzar", "+47 555 65 555", "RaniaE@mail.com", "ZA7Q118LU", "high", 1);
