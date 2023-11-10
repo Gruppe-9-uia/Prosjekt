4a000e918d40cd025a435549c9cd5f13038d63ba
Andrine Celine Flatby <andrine.celine.flatby@gmail.com>
2023-11-10 17:18:07 +0100

    Fikset på feilene i cmd, docker og sh filene som kjører riktig

--- startDb.cmd
+++ startDb.cmd
@@ -1,2 +1,2 @@
-docker container run --rm -it -d --name webapp --publish 80:80 webapp
+docker container run --rm -it -d --name prosjekt --publish 80:80 prosjekt
 docker run --rm --name mariadb -p 3308:3306/tcp -v "%cd%\database":/var/lib/mysql -e MYSQL_ROOT_PASSWORD=12345 -d mariadb:latest
