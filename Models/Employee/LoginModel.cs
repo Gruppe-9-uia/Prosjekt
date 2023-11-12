d642c1ad7e9a5dc74e4ccaacaaff923a7610e65e
Andrine Celine Flatby <andrine.celine.flatby@gmail.com>
2023-11-12 04:59:34 +0100

    Laget egent view for glemt passord, samt model som samsvare med dette

---
+++ Models/Employee/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Prosjekt.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Email_str { get;set; }
+        [Required]
+        public string Password_str { get; set; }
+
+
+    }
+}
