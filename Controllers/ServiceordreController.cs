decaf093061a1ef25823d0977a56518e0e4437c2
ineant <inea@uia.no>
2023-11-20 20:09:05 +0100

    ugh

--- Controllers/ServiceordreController.cs
+++ Controllers/ServiceordreController.cs
@@ -5,8 +5,16 @@ namespace Prosjekt.Controllers
 {
     public class ServiceordreController : Controller
     {
+        private readonly ProsjektContext _context;
+   
+        public ServiceordreController(ProsjektContext context)
+        {
+            _context = context;
+        
+        }
         public IActionResult Serviceordre()
         {
+            
             return View();
         }
 
@@ -14,13 +22,33 @@ namespace Prosjekt.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Save(ServiceOrderModel model)
         {
-            if (ModelState.IsValid)
+            Console.WriteLine("halleluja");
+            if (!ModelState.IsValid)
             {
-                var s = "ineedabreakpoint";
+           
+                var temp = new ServiceOrderModel{};
+                temp.CustomerID_int = 1;
+                temp.Order_type_str = "vinsj";
+                temp.Received_Date = new DateOnly(2023,11,20);
+                temp.Description_From_Customer_str = "funker ikke";
+                temp.OrderID_int = 11;
+                var result = _context.Service_ordre.Add(temp);
+                if (result != null)
+                {
+                    Console.WriteLine("YAYA");
+                } else
+                {
+                    Console.WriteLine("NANA");  
+                }
+
+                _context.SaveChanges();
+                
+     
+                
+                
 
             }
             return View("Serviceordre", model);
         }
     }
 }
-
