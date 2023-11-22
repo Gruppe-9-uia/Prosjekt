f08287fd51198e21c32888fcbff304095d004e79
Andrine Celine Flatby <andrine.celine.flatby@gmail.com>
2023-11-22 09:13:46 +0100

    Bruker vises i oversikt nu

--- Controllers/BrukeroversiktController.cs
+++ Controllers/BrukeroversiktController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Prosjekt.Models.Employee;
 
 namespace Prosjekt.Controllers
 {
@@ -17,11 +18,44 @@ namespace Prosjekt.Controllers
         // GET: /<controller>/
         public IActionResult Brukeroversikt()
         {
-            var employees = _context.Users.ToList().ToString();
+            try
+            {
+                var employeeList = _context.Employees.ToList();
 
+                List<BrukerOversiktViewModel> list = new List<BrukerOversiktViewModel>();
+
+                // Convert the fetched data to the view model type
+                foreach (var e in employeeList)
+                {
+                    var userRoleId = _context.UserRoles.Where(x => x.UserId.Equals(e.Id)).FirstOrDefault();
+                    if (userRoleId == null)
+                    {
+                        ModelState.AddModelError(string.Empty, "Feil å hente rolle");
+                    }
+
+                    var employeeObj = new BrukerOversiktViewModel
+                    {
+                        ID_int = e.Id,
+                        FirstName_str = e.FirstName_str,
+                        LastName_str = e.LastName_str,
+                        Department = userRoleId.RoleId
+
+                    };
+                    list.Add(employeeObj);
+                }
+
+                if (list.Count > 0)
+                {
+                    ViewData["List"] = list;
+
+                }
+                return View();
+
+            } catch (Exception ex) { 
+                Console.WriteLine(ex);
+                return View();
+            }
             
-            
-            return View();
         }
 
         public IActionResult BackBrukeroversikt()
