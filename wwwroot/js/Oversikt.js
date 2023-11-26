ae2fab2ecf1fe07dfa488d7b499c4b4092990fa5
ineant <inea@uia.no>
2023-11-26 13:18:04 +0100

    fix: fikset på søkebar, for å filtrere radene

--- wwwroot/js/Oversikt.js
+++ wwwroot/js/Oversikt.js
@@ -1,4 +1,4 @@
-
+//oversikt siden: 
     function myFunctionOversikt() {
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("søkebarOversikt");
@@ -18,4 +18,33 @@
 }
 }
 }
+}
+
+
+//brukeroversiktsiden: 
+function myFunctionBrukerOversikt() {
+    var input, filter, table, tr, td0, td1, td2, i, txtValue0, txtValue1, txtValue2;
+    input = document.getElementById("brukeroversiktInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("partsTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td0 = tr[i].getElementsByTagName("td")[0];
+        td1 = tr[i].getElementsByTagName("td")[1];
+        td2 = tr[i].getElementsByTagName("td")[2];
+
+        if (td0 && td1 && td2) {
+            txtValue0 = td0.textContent || td0.innerText;
+            txtValue1 = td1.textContent || td1.innerText;
+            txtValue2 = td2.textContent || td2.innerText;
+
+            // Check if the filter matches any of the columns
+            if (txtValue0.toUpperCase().indexOf(filter) > -1 || txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
 }
